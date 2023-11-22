9064ee4ab3c5c4d6a26c23179b7a35119d003146
Alejandro Montals Pozzoli <amontals83.work@gmail.com>
2023-11-22 17:50:27 +0100

    Creacion de la Clase Tools con la funcion ValidaUsuario

---
+++ Tools.cs
@@ -0,0 +1,31 @@
+﻿using Login.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Login
+{
+    public class Tools
+    {
+        private readonly DemoContext _context;
+
+        public Tools(DemoContext context)
+        {
+            _context = context;
+        }
+
+        public int ValidaUsuario(Usuario model)
+        {
+            try
+            {
+                var query = $"SELECT COUNT(*) FROM Usuarios WHERE Email = '{model.Email}' AND Password = '{model.Password}'";
+                var result = _context.Database.ExecuteSqlRaw(query);
+
+                return result > 0 ? 1 : 0;
+            }
+            catch
+            {
+                Exception e;
+            }
+            return 0;
+        }
+    }
+}
