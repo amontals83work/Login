78663948b2ee84569682498d14d75699b54de56c
Alejandro Montals Pozzoli <amontals83.work@gmail.com>
2023-11-22 17:49:13 +0100

    Creacion DemoConext

---
+++ Models/DemoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Login.Models
+{
+    public class DemoContext : DbContext
+    {
+        public DemoContext(DbContextOptions<DemoContext> options) : base(options) { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
