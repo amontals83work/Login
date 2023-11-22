028d1fef6b0f852a9171982b55aafb030d33b2ab
Alejandro Montals Pozzoli <amontals83.work@gmail.com>
2023-11-22 17:50:49 +0100

    Subidas miscelaneas

--- obj/Debug/net8.0/Login.AssemblyInfo.cs
+++ obj/Debug/net8.0/Login.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Login")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6fb512432c58c03bdc31d989675b0e6e4a67c3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Login")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Login")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
